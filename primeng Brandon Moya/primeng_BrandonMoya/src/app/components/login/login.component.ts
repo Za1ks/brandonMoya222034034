e5b96b71aaf0ccd1c6131f46f0d34d60c27dcda8
Jack Reacher <moyabec@gmail.com>
2024-10-26 11:15:52 -0500

    feat: capturar datos

--- primeng Brandon Moya/primeng_BrandonMoya/src/app/components/login/login.component.ts
+++ primeng Brandon Moya/primeng_BrandonMoya/src/app/components/login/login.component.ts
@@ -1,17 +1,44 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CardModule, ButtonModule],
+  imports: [PasswordModule,InputTextModule,CommonModule,CardModule,ReactiveFormsModule,ButtonModule,ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  constructor(private router: Router) {}
+  userForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private router: Router){
+    this.userForm = this.fb.group({
+
+      email:['', [Validators.required, Validators.email]],
+      password: ['',[Validators.required]],
+
+
+    });
+
+
+  }
+
+  onSubmit(){
+    if(this.userForm.valid){
+      console.log(this.userForm.value);
+    }else{
+      console.log('Formulario invalido');
+    }
+  }
+
   redirectToRegister() {
     this.router.navigate(['/register']);
   }
 }
+  
+
--- primeng Brandon Moya/primeng_BrandonMoya/src/app/components/register/register.component.ts
+++ primeng Brandon Moya/primeng_BrandonMoya/src/app/components/register/register.component.ts
@@ -1,16 +1,43 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ButtonModule,CardModule],
+  imports: [PasswordModule,InputTextModule,CommonModule,CardModule,ReactiveFormsModule,ButtonModule,ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-  constructor(private router: Router) {}
+  userForm: any;
+  constructor(private fb: FormBuilder, private router: Router){
+    this.userForm = this.fb.group({
+      name: ['', Validators.required],
+      email:['', [Validators.required, Validators.email]],
+      password: ['',[Validators.required]],
+      confirmPassword: ['', Validators.required] // Asegúrate de que este control esté aquí
+      }, { validators: this.passwordMatchValidator });
+  }
+
+  passwordMatchValidator(form: FormGroup) {
+    return form.get('password')?.value === form.get('confirmPassword')?.value
+      ? null : { mismatch: true };
+  }
+
+  onSubmit(){
+    if(this.userForm.valid){
+      console.log(this.userForm.value);
+    }else{
+      console.log('Formulario invalido');
+    }
+  }
+
+  
 
   redirectToLogin() {
     this.router.navigate(['/login']);
