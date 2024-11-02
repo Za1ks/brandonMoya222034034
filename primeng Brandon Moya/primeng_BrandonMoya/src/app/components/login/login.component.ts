dce2c7f4cc14cb256e75279161ef7355d19f5a8d
Jack Reacher <moyabec@gmail.com>
2024-11-02 11:30:00 -0500

    Clase 2/11/2024

--- primeng Brandon Moya/primeng_BrandonMoya/src/app/components/login/login.component.ts
+++ primeng Brandon Moya/primeng_BrandonMoya/src/app/components/login/login.component.ts
@@ -6,6 +6,7 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { Router } from '@angular/router';
 import { InputTextModule } from 'primeng/inputtext';
 import { PasswordModule } from 'primeng/password';
+import { LoginService } from '../../services/login.service';
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -16,7 +17,7 @@ import { PasswordModule } from 'primeng/password';
 export class LoginComponent {
   userForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private router: Router){
+  constructor(private fb: FormBuilder, private router: Router, private loginservice:LoginService){
     this.userForm = this.fb.group({
 
       email:['', [Validators.required, Validators.email]],
@@ -30,6 +31,8 @@ export class LoginComponent {
 
   onSubmit(){
     if(this.userForm.valid){
+      const {email,password} = this.userForm.value;
+      this.loginservice.login(email,password).subscribe(respinse => {console.log("Exitoso",Response)})
       console.log(this.userForm.value);
     }else{
       console.log('Formulario invalido');
